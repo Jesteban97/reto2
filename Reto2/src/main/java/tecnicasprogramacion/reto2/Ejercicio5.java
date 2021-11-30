97ef1ff504d04bfd23341cc2a4ee5a0b2d635836
Jose Waldo <josferw@gmail.com>
2021-11-30 16:20:21 -0500

    Ejercicio #3 integrado en el menú.

--- Reto2/src/main/java/tecnicasprogramacion/reto2/Ejercicio5.java
+++ Reto2/src/main/java/tecnicasprogramacion/reto2/Ejercicio5.java
@@ -21,6 +21,8 @@ public class Ejercicio5 {
                 ejercicio1();
             } else if (eleccionUsuarioMenú == 2) {
                 ejercicio2();
+            } else if (eleccionUsuarioMenú == 3) {
+              ejercicio3();
             } else if (eleccionUsuarioMenú == 4) {
                 ejercicio4();
             } else if (eleccionUsuarioMenú == 5) {
@@ -80,7 +82,27 @@ public class Ejercicio5 {
 
         System.out.println("El cuadrado del número es: " + resultado);
     }
-
+    
+    public static void ejercicio3() {
+        int N =0;
+        System.out.println("ingrese el numero N");
+        Scanner num = new Scanner(System.in);
+        N=num.nextInt();
+        int impar = -1;
+        for(int i=1; i<=N;i++){
+          impar+=2;
+          int contador = impar;
+            System.out.print("los numeros impares sumados son "+impar+" ");
+          for(int j=2;j<=i;j++){
+              impar+=2;
+              contador+= impar; 
+              System.out.print(" "+impar+" ");
+          }
+            
+            System.out.println("\nel cubo del numero "+i+" es: "+contador);
+        }
+    }
+    
     public static void ejercicio4() {
 
         Scanner lectorUser = new Scanner(System.in);
